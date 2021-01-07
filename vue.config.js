db9ce972b0cb73e0ae7b8f595e55c75631fdc0d0
rubo <rubo@e-u.cn>
2021-01-07 16:16:43 +0800

    disableHostCheck: true

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     productionSourceMap: false,
      devServer: {
          port: 8824,     // 端口号
+		 disableHostCheck: true,
     //     proxy: {
     //         '/api':{
     //             target:'http://jsonplaceholder.typicode.com',
