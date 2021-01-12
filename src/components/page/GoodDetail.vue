11040b857f6e6a5f03477c997f314b302de8e931
rubo <rubo@e-u.cn>
2021-01-12 17:17:20 +0800

    添加文件上传控件

--- src/components/page/GoodDetail.vue
+++ src/components/page/GoodDetail.vue
@@ -61,7 +61,19 @@
                         <el-input v-model="goodData.goods_unit"></el-input>
                     </el-form-item>
                     <el-form-item label="简要图">
-                        <el-input v-model="goodData.primary_pic_url"></el-input>
+                        <!--el-input v-model="goodData.primary_pic_url"></el-input!-->
+                        
+                    <div class="container">
+                        <el-upload
+                            class="upload-demo"
+                            drag
+                            action="https://www.ylhzzy.top:8826/img/"
+                            multiple>
+                            <i class="el-icon-upload"></i>
+                            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                        </el-upload>
+                    </div>
                     </el-form-item>
                     <el-form-item label="list_pic_url">
                         <el-input v-model="goodData.list_pic_url"></el-input>
